51326b5fe67a1980ce7e542826fae043d2fd455f
Emre Can Ayar <emrecan.ayar@hotmail.com>
2023-05-07 23:01:32 +0300

    Basic ElasticSearch Configuration

---
+++ ElasticSearch/ElasticSearch.ConsoleApp/MyDocument.cs
@@ -0,0 +1,10 @@
+﻿namespace ElasticSearch.ConsoleApp
+{
+    //Örnek verinin tutulacağı model
+    public class MyDocument
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ ElasticSearch/ElasticSearch.ConsoleApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using ElasticSearch.ConsoleApp;
+using Nest;
+
+//ElasticSearch sunucuna bağlanmak.
+ConnectionSettings settings = new ConnectionSettings(new Uri("http://localhost:9200"))
+    .DefaultIndex("myindex");
+
+//Belirtilen bağlantı ayarlarına göre bir client nesnesi oluşturalım.
+var client = new ElasticClient(settings);
+
+//ElasticSearch içerisinde saklanacak olan örnek bir veri.
+var myDocument = new MyDocument
+{
+    Id = 1,
+    Title = "ElasticSearch Öğrenme",
+    Content = "ElasticSearch güçlü bir arama motorudur."
+};
+
+//Oluşturulan veriyi elasticSearch üzerinde indexledik.
+var indexResponse = await client.IndexDocumentAsync(myDocument);
+
+
+//İndexlenen veriler üzerinden bir arama işlemi gerçekleştirelim.
+
+string keyword = "ElasticSearch";
+
+/*
+ 
+ Bu örnekte, keyword adlı değişken aranacak anahtar kelimeyi içerir. From ve Size parametreleri, sonuçların sayfalanmasını sağlar. Bu örnekte, ilk sayfada en fazla 10 sonuç döndürülür.
+ 
+ */
+
+//Arama işlemini SearchAsync fonksiyonu üzerinden sayfalama işlemi yaparak gerçekleştirebiliriz.
+var searchResponse = await client.SearchAsync<MyDocument>(s => s
+    .From(0)
+    .Size(10)
+    .Query(q => q
+         .Match(m => m
+            .Field(f => f.Title)
+            .Query(keyword)
+         )
+    )
+);
+
+
+if (searchResponse.IsValid)
+{
+    Console.WriteLine($"Toplam {searchResponse.Total} sonuç bulundu.");
+
+    foreach (var hit in searchResponse.Hits)
+    {
+        Console.WriteLine($"Belge Id: {hit.Id} - Başlık: {hit.Source.Title}");
+    }
+}
+else
+{
+    Console.WriteLine("Sorgu başarısız oldu: " + searchResponse.DebugInformation);
+}
